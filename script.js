16e660b2f069838b613ce636c78702584a3dbc60
Tejas <tejaskkadam3003@gmail.com>
2023-12-13 21:54:43 +0530

    big update

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 let userScore = 0;
 let compScore = 0;
+let clickCounter = 0;
+const maxClicks = 5; // Adjust this value to the desired number of clicks.
+
+const resetTable = () => {
+    const table = document.querySelector('table');
+    // Remove all rows from the table except the header row (first row).
+    for (let i = table.rows.length - 1; i > 0; i--) {
+        table.deleteRow(i);
+    }
+};
+
+const resetTableAndCounter = () => {
+    resetTable();
+    clickCounter = 0;
+};
+
+
+
+
 
 const choices = document.querySelectorAll(".choice");
 const msg =  document.querySelector("#msg");
 const userScorePara = document.querySelector("#user_score");
 const compScorePara = document.querySelector("#comp_score");
+
+const user_selector = document.querySelector(".user_selector");
+const comp_selector = document.querySelector(".comp_selector");
+
+// const selectors = document.querySelector(".selector2");
 
 const generateCompChoice = () => {
     const options = ["rock", "paper", "scissor"];
@@ -14,12 +38,16 @@ const generateCompChoice = () => {
 
 }  
 
+
 const drawGame = () => {
     console.log("Game was Draw");
     msg.innerText = "Game Draw Play Again!";
     msg.style.backgroundColor = "Black";
+    // updateTable(null, null); 
+    
 
 }
+
 
 const showWinner = (userWin, userChoice, compChoice) => {
 
@@ -37,16 +65,44 @@ const showWinner = (userWin, userChoice, compChoice) => {
         msg.innerText = `You lost ${compChoice} beats your ${userChoice}`;
         msg.style.backgroundColor = "Red";
     }
+    
 };
+
+const updateTable = (userChoice, compChoice) => {
+    const table = document.querySelector('table');
+    const newRow = table.insertRow(1);
+
+    
+
+    const userCell = newRow.insertCell(0);
+    const compCell = newRow.insertCell(1);
+
+    userCell.textContent = userChoice;
+    compCell.textContent = compChoice;
+
+    clickCounter++;
+
+    if (clickCounter === maxClicks) {
+        resetTableAndCounter(); // Reset the table and counter after the specified number of clicks.
+    }
+};
+
+
 
 const playGame = (userChoice) =>{
 
    
     console.log("user Choice = ", userChoice);
+    user_selector.innerText = userChoice; 
+    // userChoice++;
+
 
     //generate Computer Choice
     const compChoice = generateCompChoice();
     console.log("comp Choice = ", compChoice);
+    comp_selector.innerText = compChoice; 
+
+    updateTable(userChoice, compChoice);
 
     if(userChoice === compChoice)
     {
@@ -66,6 +122,7 @@ const playGame = (userChoice) =>{
 
         }
         showWinner(userWin, userChoice, compChoice);
+        updateTable(userChoice, compChoice);
         
 
     }
